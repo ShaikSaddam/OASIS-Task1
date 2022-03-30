54003356501475a29041e566131bbeb9d0002426
Shaik Saddam Hussain <92745355+ShaikSaddam@users.noreply.github.com>
2022-03-30 13:40:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+
+function clr() {
+    document.getElementById("result").value = ""
+}
+
+function del() {
+    let input = document.getElementById("result");
+    input.value = input.value.substring(0, input.value.length - 1);
+}
